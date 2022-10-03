2abe43d1289d6b6ff1944ac5dc2904b109efe3f6
Abdurehman Saeed <asaeed526@gmail.com>
2022-10-03 13:56:24 +0300

    css and js logic edited

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,52 +1,42 @@
 import React, { useState } from 'react'
 import Buttons from './Buttons'
 import './styles/board.css'
+let newString=''
+let value=0
 
 const Board = () => {
 
     let [btnValue,setValue]=useState('')
     let [topValue,setTopValue]=useState('')
-    let array=['%','CE','C','1/x','x2','square','7','8','9','4','5','6','3','2','1','+/-','0','.']
-    // let array2=['Delete','/','C','x','-','+','=']
+  
+    
+    let array=['7','8','9','4','5','6','3','2','1','+/-','0','.']
+  
 
 const handleEvaluation =(e)=>{
       
-    if(btnValue===''){
-         btnValue='0'
-      }
-      
-      if(topValue.includes('+')){
-         handleAddition(e)
-
-      }else if(topValue.includes('*')){
-        if(btnValue===''){
-            btnValue='1'
-        }
-        handleMultiplication(e)
-     
-      }else if(topValue.includes('/')){
-        if(btnValue===''||btnValue==='0'){
-            setValue("Cannot be divided by zero");
-        }else handleDivision(e)
-        
-      }else if(topValue.includes('-')){
-            handleSubtraction(e)
-      }else if(topValue.includes('%')){
-            handleModule(e)
-      }
-      else{   
+   if(btnValue==='') btnValue='0'
+   if(topValue.includes('+'))handleAddition(e)
+   else if(topValue.includes('*'))handleMultiplication(e)
+   else if(topValue.includes('/')){
+        if(btnValue===''||btnValue==='0')setValue("Cannot be divided by zero");
+        else handleDivision(e)
+   }else if(topValue.includes('-'))handleSubtraction(e)
+    else if(topValue.includes('%')) handleModule(e)
+    else{   
       setTopValue(btnValue+=e.target.value)
       setValue('')
     }
       
   }
 
-  const handleCancel=()=>{
+
+const handleCancel=()=>{
     setValue("0")
     setTopValue('')
-  }
+}
  
-  let newString=''
+
 
 const handleDelete=()=>{
     let value = btnValue.split('')
@@ -58,62 +48,123 @@ const handleDelete=()=>{
   
 }
 
-const handleParseInt=(value)=>{
-    return parseInt(value)
+
+const handleParseFloat=(value)=>{
+    return parseFloat(value)
 }
 
 
 const handleAddition =(e)=>{
 
-        let value =handleParseInt(topValue) + handleParseInt(btnValue)
+        value = handleParseFloat(topValue) + handleParseFloat(btnValue)
         setTopValue(value.toString().concat(e.target.value))
-        setValue('')
+        setValue(value.toString())
 }
 
+
 const handleSubtraction =(e)=>{
-    
-        let value =handleParseInt(topValue) - handleParseInt(btnValue)
+      
+        value = handleParseFloat(topValue) - handleParseFloat(btnValue)
         setTopValue(value.toString().concat(e.target.value))
-        setValue('')
+        setValue(value.toString())
 }
+
+
 
 const handleMultiplication =(e)=>{
     
-        let value =handleParseInt(topValue) * handleParseInt(btnValue)
+        value = handleParseFloat(topValue) * handleParseFloat(btnValue)
         setTopValue(value.toString().concat(e.target.value))
-        setValue('')
+        setValue(value.toString())
 }
+
 
 const handleDivision =(e)=>{
-    
-        let value =handleParseInt(topValue) / handleParseInt(btnValue)
+   
+        value = handleParseFloat(topValue) / handleParseFloat(btnValue)
         setTopValue(value.toString().concat(e.target.value))
-        setValue('')
+        setValue(value.toString())
 }
 
+
 const handleModule =(e)=>{
-        let value =handleParseInt(topValue) % handleParseInt(btnValue)
+        value = handleParseFloat(topValue) % handleParseFloat(btnValue)
         setTopValue(value.toString().concat(e.target.value))
-        setValue('')
+        setValue(value.toString())
+}
+
+
+
+const handleRatio =()=>{
+        
+       if(btnValue===''){
+        setValue('can not be divided by zero ')
+       }else{
+        let value =1/ handleParseFloat(btnValue)
+        alert(value)
+        setValue(value.toString())
+        
+      }
+}
+
+
+
+const handleSquare =()=>{
+        
+       if(btnValue===''){
+         btnValue='0'
+       }
+        let value =Math.pow(handleParseFloat(btnValue),2)
+        setValue(value.toString())
+        
+      
+}
+
+const handleSquareRoot =()=>{
+        
+       if(btnValue===''){
+         btnValue='0'
+       }
+        let value =Math.sqrt(handleParseFloat(btnValue))
+        setValue(value.toString())
+        
+      
+}
+const handleNegation =()=>{
+        
+       if(btnValue==='' || '0'){
+         setValue('')
+       }
+        let value =-1*handleParseFloat(btnValue)
+        setValue(value.toString())
+      
 }
 
   return (
+
 <div className='board-container'>
- <div className="value-display">
+ 
+   <div className="value-display">
      <div className="top">{topValue}</div>
      <div className="bottom">{btnValue}</div>
    </div>
 
-     <div className="btn-container">
-        <div className="left-container">
-        
-        {array.map(item=>
+    <div className="btn-container">
+     <div className="left-container">
+      <button onClick={handleEvaluation} value={'%'}>{'%'}</button>
+       <button onClick={handleCancel}>{'CE'}</button>
+       <button onClick={handleCancel}>{'C'}</button>
+       <button onClick={handleRatio}>{'1/X'}</button>
+       <button onClick={handleSquare}>{'X2'}</button>
+       <button onClick={handleSquareRoot} >{'root'}</button>
+      
+       {array.map(item=>
          <Buttons
           topValue={topValue} 
           btnValue={btnValue} 
           setValue={setValue} 
           value={item}
-          handleCancel={handleCancel}
+          handleNegation={handleNegation}
          />)
         }
         </div>
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './styles/button.css'
 
-const Buttons = ({value,topValue,handleCancel,btnValue,setValue}) => {
+const Buttons = ({value,topValue,handleNegation,btnValue,setValue}) => {
   return (
     <button 
     className='button' 
-    onClick={value==='C'? handleCancel:(e)=>setValue((btnValue+=e.target.value))}
+    onClick={value==='+/-'? handleNegation:(e)=>setValue((btnValue+=e.target.value))}
     value={value}>{value}</button>
   )
 }
